cbed050b5bf6d520dcd69a146a9d07653135d8b4
carlosoliveira-neves1 <carlos.oliveiraneves1@gmail.com>
2025-07-11 13:15:31 -0300

    fix(firebase): corrige storageBucket para .appspot.com

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 // src/firebase.js
-import { initializeApp }  from "firebase/app";
-import { getFirestore }   from "firebase/firestore";
-import { getStorage }     from "firebase/storage";
+import { initializeApp }    from "firebase/app";
+import { getFirestore }     from "firebase/firestore";
+import { getStorage }       from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "…",
+  apiKey: "AIzaSyCLQ7qY_C0uoDE6klRKorbbyAutcY9w03E",
   authDomain: "b2portifolio.firebaseapp.com",
   projectId: "b2portifolio",
-  storageBucket: "b2portifolio.appspot.com",
+  storageBucket: "b2portifolio.appspot.com",    // ← corrige aqui
   messagingSenderId: "37098713136",
   appId: "1:37098713136:web:27ea6a9a1efd7a23f97df5",
   measurementId: "G-QEXYY8F5M2"
